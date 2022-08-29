457a0ad588d190d5c1bda3a1adc328860a706861
vulcan-dev <vulcan-dev@proton.me>
2022-08-29 03:02:24 +0100

    change sizeof

--- src/vklib_compat.c
+++ src/vklib_compat.c
@@ -13,7 +13,7 @@ const char* get_error(const char* usr_msg, int err_code) {
         sys_msg[sizeof sys_msg - 1] = '\0';
     }
 #else
-    if (strerror_r(err_code, sys_msg, sizeof sys_msg) != 0) {
+    if (strerror_r(err_code, sys_msg, sizeof(sys_msg)) != 0) {
         strncpy(sys_msg, "Unknown error", sizeof "Unknown error");
         sys_msg[sizeof sys_msg - 1] = '\0';
     }
